0a8b4d84b83abccb29ea7b0a941560d4d3a44f6b
hucj <hucj@funshion.com>
2013-09-12 14:44:46 +0800

    add send email test

--- toollib/tests.py
+++ toollib/tests.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import time
-
+import os
 from django.core import mail
 from django.test import TestCase
 from toollib.email import send_mail, send_html_template_email
+
 
 
 class EmailTestCases(TestCase):
@@ -26,7 +27,17 @@ class EmailTestCases(TestCase):
         self.assertEquals(mail.outbox[0].body, 'body_use_thread')
 
     def test_send_html_template_email(self):
-        send_html_template_email("test", 'email.html', {'username':'test'}, ["hucj@funshion.com"], [], False)
+        send_html_template_email("template_subject_not_use_thread", 'email.html', {'username':'test'}, ["hucj@funshion.com"], [], False)
+        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(mail.outbox[0].subject, 'template_subject_not_use_thread')
+        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test</strong>\r\nyour account activated.\r\n</body>')
+
+    def test_send_html_template_email_use_thread(self):
+        send_html_template_email("template_subject_use_thread", 'email.html', {'username':'test'}, ["hucj@funshion.com"], [], True)
+        time.sleep(1)
+        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(mail.outbox[0].subject, 'template_subject_use_thread')
+        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test</strong>\r\nyour account activated.\r\n</body>')
 
 class RenderTestCases(TestCase):
 
