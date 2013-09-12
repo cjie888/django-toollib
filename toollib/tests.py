c3ef0f23f2bcabb08bf33ca8533bc8b6b4a07b1c
hucj <hucj@funshion.com>
2013-09-12 15:44:01 +0800

    add render template test

--- toollib/tests.py
+++ toollib/tests.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import time
-import os
+
 from django.core import mail
 from django.test import TestCase
 from toollib.email import send_mail, send_html_template_email
+from toollib.render import render_json, render_template
 
 
 
@@ -30,16 +31,22 @@ class EmailTestCases(TestCase):
         send_html_template_email("template_subject_not_use_thread", 'email.html', {'username':'test'}, ["hucj@funshion.com"], [], False)
         self.assertEquals(len(mail.outbox), 1)
         self.assertEquals(mail.outbox[0].subject, 'template_subject_not_use_thread')
-        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test</strong>\r\nyour account activated.\r\n</body>')
+        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test</strong>\r\nyour account activated.\r\n</body>\r\n</html>')
 
     def test_send_html_template_email_use_thread(self):
-        send_html_template_email("template_subject_use_thread", 'email.html', {'username':'test'}, ["hucj@funshion.com"], [], True)
+        send_html_template_email("template_subject_use_thread", 'email.html', {'username':'test_use_thread'}, ["hucj@funshion.com"], [], True)
         time.sleep(1)
         self.assertEquals(len(mail.outbox), 1)
         self.assertEquals(mail.outbox[0].subject, 'template_subject_use_thread')
-        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test</strong>\r\nyour account activated.\r\n</body>')
+        self.assertEquals(mail.outbox[0].body, u'<html>\r\n<body>\r\nhello <strong>test_use_thread</strong>\r\nyour account activated.\r\n</body>\r\n</html>')
 
 class RenderTestCases(TestCase):
+    """test render
 
+    """
     def test_render_json(self):
         pass
+    
+    def test_render_template(self):
+        response = render_template('email.html', {'username' : 'render_test'})
+        self.assertEquals(response.content, '<html>\r\n<body>\r\nhello <strong>render_test</strong>\r\nyour account activated.\r\n</body>\r\n</html>')
