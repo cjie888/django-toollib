1102abc324529e19527f27cf59612dc5659d58a5
hucj <hucj@funshion.com>
2013-09-17 15:49:51 +0800

    add page module

---
+++ toollib/page.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def get_page(query_set, page_no, page_size):
+    paginator = Paginator(query_set, page_size) 
+    try:
+        page = paginator.page(page_no)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        page = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        page = paginator.page(paginator.num_pages)
+    return page
