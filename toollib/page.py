6bc9127e576174bc452e610d8dc06d9d5d0a76ca
hucj <hucj@funshion.com>
2013-09-18 17:57:52 +0800

    change comment

--- toollib/page.py
+++ toollib/page.py
@@ -9,6 +9,6 @@ def get_page(query_set, page_no, page_size):
         # If page is not an integer, deliver first page.
         page = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
+        # If page is out of range (e.g. 9999), deliver first page.
         page = paginator.page(1)
     return page
